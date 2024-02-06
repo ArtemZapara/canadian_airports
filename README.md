59114db0b49cecaa2cf46e49ddbc1fcd0c279902
Artem Zapara <art.zapara@gmail.com>
2024-02-06 12:38:16 +0200

    added new data source (table 23100302)

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Canadian airports - Streamlit application [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://artemzapara-canadian-airports-app-zgl7n2.streamlitapp.com)
+# Canadian airports - Streamlit application [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://canadian-airports.streamlit.app)
 
 This repository will host the statistics on aircraft movements for major Canadian airports in a form of a Streamlit web application. The application consists of two types of interactive dashboards. The first one is a bar chart with historical data for each Canadian airport with NAV CANADA towers starting from January 1997. The second dashboard shows the top 10 airports in each aircraft movement category during the given time period.
 
-The raw data is taken from the following source: Statistics Canada. Table 23-10-0008-01 Domestic and international Itinerant movements, by type of operation, airports with NAV CANADA towers, monthly (https://doi.org/10.25318/2310000801-eng).
+The raw data is taken from the following sources:
+* Statistics Canada. **Table 23-10-0302-01**. Domestic and international itinerant movements, by type of operation, at airports with NAV CANADA services and other selected airports, monthly (https://doi.org/10.25318/2310030201-eng).
+* Statistics Canada. **Table 23-10-0008-01**. Domestic and international Itinerant movements, by type of operation, airports with NAV CANADA towers, monthly (https://doi.org/10.25318/2310000801-eng).
--- processing/process_movement_data.py
+++ processing/process_movement_data.py
@@ -10,14 +10,51 @@ def process_data(df):
     ]
     return df[cols]
 
+def compute_total(df):
+    airports = df[df["Airports"] != "Total, all airports"]
+
+    total = airports.groupby(["Domestic and international itinerant movements", "Type of operation", "REF_DATE"]).sum().reset_index()
+
+    total["Airports"] = "Total, all airports"
+
+    return pd.concat([airports, total], ignore_index=True)
+
 if __name__ == "__main__":
-    movement_data = pd.read_csv(
+    old_data = pd.read_csv(
         "../data/23100008.csv",
         dtype={
             "TERMINATED": "str"
         }
     )
+    new_data = pd.read_csv(
+        "../data/23100302.csv",
+        dtype={
+            "TERMINATED": "str"
+        }
+    )
 
-    movement_data = process_data(movement_data)
-    movement_data["Airports"] = movement_data["Airports"].str.replace("\n","")  # to handle the Red Deer Regional case
+
+    old_data = process_data(old_data)
+    old_data = old_data[old_data["REF_DATE"] < "2019-01"]
+
+    # Handling some special cases
+    old_data["Airports"] = old_data["Airports"].str.replace("\n","")
+    old_data["Airports"] = old_data["Airports"].str.replace("Whitehorse International, Yukon","Whitehorse/Erik Nielsen International, Yukon")
+
+    old_airports = old_data["Airports"].unique().tolist()
+
+    new_data = process_data(new_data)
+    new_data["VALUE"] = new_data["VALUE"].fillna(0).astype(int)
+    new_data = new_data[new_data["Airports"].isin(old_airports)]
+
+    old_total = compute_total(old_data)
+    new_total = compute_total(new_data)
+
+    movement_data = pd.concat([old_total, new_total], ignore_index=True).sort_values(by=[
+             "REF_DATE",
+             "Airports",
+             "Domestic and international itinerant movements",
+             "Type of operation"
+    ])
+
     movement_data.to_csv("../data/movement_data.csv", index=False)
